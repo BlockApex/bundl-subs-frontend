f80ac9d00ad7f68eb553eba7321f1ef30a693e64
Hamza Khan <hamzakhann66@gmail.com>
2025-10-25 15:43:14 +0500

    changed url

--- client/src/app/config/index.ts
+++ client/src/app/config/index.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 
 // export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https:172.18.0.33:2000';
 
-// export const BASE_URL = 'https://server-production-fa9f.up.railway.app';
-export const BASE_URL = 'https://172.18.0.33';
+export const BASE_URL = 'https://server-production-fa9f.up.railway.app';
+// export const BASE_URL = 'https://172.18.0.33';
 
 export const Api = axios.create({
   baseURL: BASE_URL,
